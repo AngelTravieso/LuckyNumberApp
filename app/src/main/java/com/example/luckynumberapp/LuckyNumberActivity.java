26d5063a09a33b5725abc20cb1ea1991788f156e
Angel Travieso <angeltraviesoc@gmail.com>
2022-11-28 10:16:05 -0400

    declarando e inicializando widgets

--- app/src/main/java/com/example/luckynumberapp/LuckyNumberActivity.java
+++ app/src/main/java/com/example/luckynumberapp/LuckyNumberActivity.java
@@ -3,12 +3,26 @@ package com.example.luckynumberapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LuckyNumberActivity extends AppCompatActivity {
+
+    // declaring widgets
+    TextView welcomeTxt, luckyNumberTxt;
+    Button share_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lucky_number);
+
+        // initialize widgets
+        welcomeTxt = findViewById(R.id.textView2);
+        luckyNumberTxt = findViewById(R.id.lucky_number_txt);
+        share_btn = findViewById(R.id.share_number_btn);
+
+        
+
     }
 }
--- app/src/main/java/com/example/luckynumberapp/MainActivity.java
+++ app/src/main/java/com/example/luckynumberapp/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.luckynumberapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // declaring widgets
+    EditText editText;
+    Button btn;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialize widgets
+        editText = findViewById(R.id.editText);
+        txt = findViewById(R.id.editText);
+        btn = findViewById(R.id.btn);
+
+
+
+
     }
 }
