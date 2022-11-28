06a76843aa3065420c3ffcfb4933437b9639bb53
Angel Travieso <angeltraviesoc@gmail.com>
2022-11-28 11:16:58 -0400

    compartiendo el nombre de usuario y su numero de la suerte

--- app/src/main/java/com/example/luckynumberapp/LuckyNumberActivity.java
+++ app/src/main/java/com/example/luckynumberapp/LuckyNumberActivity.java
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.Random;
 
@@ -34,20 +34,47 @@ public class LuckyNumberActivity extends AppCompatActivity {
         // Random Number Generated
         int randomNumber = generateRandomNumber();
 
+        // set luckyNumber to the textView
         luckyNumberTxt.setText("" + randomNumber);
 
 
+        // action for the share button
+        share_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareData(userName, randomNumber);
+            }
+        });
+
     }
 
+
+    // Generate Random Number
     public int generateRandomNumber() {
         // Random class
         Random random = new Random();
         int upper_limit = 1000;
 
         // 1000 is not included
-        int randomNumberGenerated = random.nextInt(upper_limit);
+        return random.nextInt(upper_limit);
+    }
 
-        return randomNumberGenerated;
+
+    // Share the data lucky number
+    public void shareData(String username, int randomNumber) {
+         // Implicit intent
+        Intent i = new Intent(Intent.ACTION_SEND);
+
+        // type of data
+        i.setType("text/plain");
+
+        // Here I need to pass the title and the body of the message
+        i.putExtra(Intent.EXTRA_SUBJECT, username); // title of the email
+        i.putExtra(Intent.EXTRA_TEXT, randomNumber); // body
+
+        // Choose platform for share the data (lucky number)
+        startActivity(Intent.createChooser(i, "Choose a platform"));
+
     }
 
 }
